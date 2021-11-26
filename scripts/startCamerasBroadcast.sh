05bc31c2ab06f6982b55a9329f707c14c61ac44e
Martin H <marhilg@student.chalmers.se>
2021-11-26 16:19:27 +0100

    Run with port 2121 when using broadcast script and no port arg

--- scripts/startCamerasBroadcast.sh
+++ scripts/startCamerasBroadcast.sh
@@ -3,7 +3,7 @@
 
 # IP address is always broadcast (i.e. 192.168.1.255 on the lab net)
 # If one argument is passed to this script, assume that it is port
-# If no argument is passed, start with broadcast IP and standard port (2020)
+# If no argument is passed, start with broadcast IP and standard port (2121)
 
 trap 'kill %1; kill %2; kill %3; kill %4' SIGINT
 if [ $# -eq 1  ]
@@ -13,10 +13,10 @@ then
   ./GulliView -d 2 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N $1 &
   ./GulliView -d 3 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N $1 &
 else
-  ./GulliView -d 0 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B &
-  ./GulliView -d 1 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B &
-  ./GulliView -d 2 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B &
-  ./GulliView -d 3 -n -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B &
+  ./GulliView -d 0 -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N 2121 &
+  ./GulliView -d 1 -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N 2121 &
+  ./GulliView -d 2 -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N 2121 &
+  ./GulliView -d 3 -f Tag16h5 -W 800 -H 448 -V 192.168.1.255 -B -N 2121 &
 fi
 
 wait
